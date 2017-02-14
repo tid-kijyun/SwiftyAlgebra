6d36798fc45c9ca422c110e9cf661b07a35d486d
Taketo Sano <taketo1024@gmail.com>
2017-02-14 14:21:08 +0900

    RealNumbers are instantiatiable from Rationals.

--- Algebra.playground/Sources/Concrete/Numbers/Real.swift
+++ Algebra.playground/Sources/Concrete/Numbers/Real.swift
@@ -3,6 +3,10 @@ import Foundation
 public typealias RealNumber = Double
 
 extension RealNumber: Field {
+    public init(_ q: RationalNumber) {
+        self.init(RealNumber(q.numerator) / RealNumber(q.denominator))
+    }
+    
     public var inverse: RealNumber {
         return 1 / self
     }
