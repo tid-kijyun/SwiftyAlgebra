b346cc6e07646bcb9233309b9c68778c929604d2
Taketo Sano <taketo1024@gmail.com>
2017-10-23 10:48:16 +0900

    impl PersistentHomology calculation.

--- Sources/Geometry/Persistence.swift
+++ Sources/Geometry/Persistence.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public struct Persistence {
+public struct PersistentComplex {
     public let filtration: [SimplicialComplex]
     public init(_ filtration: [SimplicialComplex]) {
         assert(!filtration.isEmpty)
@@ -66,9 +66,42 @@ public struct Persistence {
         let chain = (0 ... dim).map{ (i) -> BoundaryMap in boundaryMap(i) }
         return ChainComplex(chain)
     }
-    
-    public func homology<K: Field>(coeffType: K.Type) -> Homology<Polynomial<K>, Simplex> {
-        return Homology(chainComplex(coeffType: K.self))
-    }
 }
 
+public struct PersistentHomology<K: Field> {
+    public let complex: PersistentComplex
+    public let homology: Homology<Polynomial<K>, Simplex>
+    
+    public init(_ filtration: [SimplicialComplex], _ type: K.Type) {
+        self.complex = PersistentComplex(filtration)
+        self.homology = Homology(complex.chainComplex(coeffType: K.self))
+    }
+    
+    public func describe(_ i: Int) -> [(birthTime: Int, deathTime: Int?, generator: FreeModule<K, Simplex>)] {
+        func birthTime(_ g: FreeModule<Polynomial<K>, Simplex>) -> Int {
+            let (s, v) = g.elements.first{ $0.value != 0 }!
+            return v.degree + complex.birthTime(of: s)!
+        }
+        
+        func asCycle(_ g: FreeModule<Polynomial<K>, Simplex>) -> FreeModule<K, Simplex> {
+            return FreeModule( g.map{ (s, v) in (v.leadCoeff, s) } )
+        }
+        
+        return homology[i].summands.map{ e in
+            switch e {
+            case let .Free(generator: g):
+                return (birthTime(g), nil, asCycle(g))
+            case let .Tor(factor: r, generator: g):
+                return (birthTime(g), birthTime(g) + r.degree, asCycle(g))
+            }
+        }
+    }
+    
+    public var detailDescription: String {
+        return (0 ... complex.dim).map { i in
+            "\(i):" + describe(i).map{ (b, d, g) in
+                "\t[\(b), \(d.flatMap{"\($0)"} ?? "∞")) : \(g)"
+            }.joined(separator: ",\n")
+        }.joined(separator: "\n\n")
+    }
+}
--- Workspace/SwiftyAlgebra/Playgrounds/Persistence.playground/Contents.swift
+++ Workspace/SwiftyAlgebra/Playgrounds/Persistence.playground/Contents.swift
@@ -9,22 +9,16 @@ typealias Z = IntegerNumber
 typealias Q = RationalNumber
 typealias R = RealNumber
 
-let V = VertexSet(count: 4)
+let V = VertexSet(count: 5)
 let s = Simplex.generator(V)
 
 let Ks = SimplicialComplex.filtration([
-    [s(0, 1), s(1, 2), s(3)],
-    [s(0, 3), s(2, 3)],
-    [s(0, 2)],
-    [s(0, 1, 2)],
-    [s(0, 2, 3)]
+    [s(1, 2), s(2, 3), s(4)],
+    [s(1, 4), s(3, 4)],
+    [s(1, 3)],
+    [s(1, 2, 3)],
+    [s(1, 3, 4)]
 ])
 
-let PH = Persistence(Ks)
-
-PH.birthTime(of: s(0))
-PH.birthTime(of: s(0, 1, 2))
-PH.birthTime(of: s(0, 1, 3))
-
-
-
+let PH = PersistentHomology(Ks, Z_2.self)
+print(PH.detailDescription)
